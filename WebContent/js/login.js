90df44a7cb4436ec2b266b4fb16eb1f7a564818f
pejapie <pejapie@gmail.com>
2016-08-26 14:16:15 +0900

    Create login.js
    메세지가 나올때 트랜지션을 주려고 작업중

---
+++ WebContent/js/login.js
@@ -0,0 +1,22 @@
+// window.onload = function(){
+// 	btnLogin.onclick = function(){
+// 		uid = document.getElementById("id");
+// 		upw = document.getElementById("password");
+
+// 		if(){
+// 			// 아이디와 비밀번호가 잘못 입력되었을 때
+// 			document.getElementById("warning").visibility="visible";
+// 		}else{
+// 			loginForm.submit();
+// 		}
+// }
+
+function loginCheck(){
+	document.getElementById("warning").style.visibility="visible";
+	document.getElementById("warningp").style.visibility="visible";
+	document.getElementById("warning").style.transition="all 2s";
+	// document.getElementById("warningp").style.transitionDuration="1";
+	// document.getElementById("warning").style.transitionDelay="1";
+	// document.getElementById("warningp").style.transitionDelay="1";
+
+}
